38556f2ea7c7d8447c0e92429fb38ef1da0eb2ab
Anna Novikova <anna.novikova.dev@gmail.com>
2025-04-18 10:16:17 +0300

    Readme description added

--- main.py
+++ main.py
@@ -5,6 +5,6 @@ bank_transactions =  [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T
                      {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
                      {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]
 
-print(filter_by_state(bank_transactions, 'CANCELED'))
+print(filter_by_state(bank_transactions))
 
 print(sort_by_date(bank_transactions))
