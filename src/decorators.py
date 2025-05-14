5b8dc322b018a9e4c5c4cbbe8a0a8a4ac88300c9
Anna Novikova <anna.novikova.dev@gmail.com>
2025-05-14 12:07:36 +0300

    decorator created

---
+++ src/decorators.py
@@ -0,0 +1,33 @@
+from functools import wraps
+from typing import Any
+
+def log(filename: Any = None):
+    """Декоратор для логирования функций и вывода результатов в консоль или файл и
+    принимающий на вход необязательный аргумент filename"""
+    def wrapper(func: Any) -> Any:
+        @wraps(func)
+        def inner(*args: Any, **kwargs: Any) -> Any:
+            try:
+                result = func(*args, **kwargs)
+                if filename:
+                    with open(filename, 'a', encoding='utf-8') as file:
+                        file.write(f"{func.__name__} ok\n")
+                else:
+                    print(f"{func.__name__} ok\n")
+                return result
+            except Exception as error:
+                if filename:
+                    with open(filename, 'a', encoding='utf-8') as file:
+                        file.write(f"{func.__name__} error: {error}. Inputs: {args}, {kwargs}\n")
+                else:
+                    print(f"{func.__name__} error: {error}. Inputs: {args}, {kwargs}\n")
+        return inner
+    return wrapper
+
+
+
+@log(filename="mylog.txt")
+def my_function(x, y):
+    return x + y
+
+my_function('line', 2)
