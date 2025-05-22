9671aac0910a808e11a842c10e326c4ca961c0b6
Anna Novikova <anna.novikova.dev@gmail.com>
2025-05-22 12:30:46 +0300

    test for utils added

--- src/utils.py
+++ src/utils.py
@@ -8,15 +8,10 @@ def convert_file(path_to_file: str) -> list:
         with open(path_to_file, 'r', encoding='utf-8') as file:
             transactions_data = json.load(file)
 
-    except FileNotFoundError:
-        return []
-
-    if not transactions_data or not isinstance(transactions_data, list):
+    except (FileNotFoundError, json.JSONDecodeError):
         return []
 
     return transactions_data
 
 
-if __name__ == "__main__":
-    print(convert_file(r'C:\Users\user\PycharmProjects\Bank_project\data\operations.json'))
 
---
+++ tests/test_external_api.py
@@ -0,0 +1,4 @@
+from src.external_api import convert_currency
+from unittest.mock import patch, Mock
+
+def test_external_api_correct
