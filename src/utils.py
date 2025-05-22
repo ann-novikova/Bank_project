c8e6961bf023b20e4ae87136cf4c238a99b93acc
Anna Novikova <anna.novikova.dev@gmail.com>
2025-05-22 10:05:13 +0300

    first function completed

---
+++ src/utils.py
@@ -0,0 +1,20 @@
+import json
+
+def convert_file(path_to_file: str) -> list:
+    """Функцию, которая принимает на вход путь до JSON-файла и возвращает список словарей
+    с данными о финансовых транзакциях"""
+
+    try:
+        with open(path_to_file, 'r', encoding='utf-8') as file:
+            transactions_data = json.load(file)
+
+    except FileNotFoundError:
+        return []
+
+    if not transactions_data or not isinstance(transactions_data, list):
+        return []
+
+    return transactions_data
+
+if __name__ == "__main__":
+    print(convert_file(r'C:\Users\user\PycharmProjects\Bank_project\data\operations.json'))
